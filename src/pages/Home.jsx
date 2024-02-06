ec3b5d29d56b09afaa5977a063efad33be9d793e
professorabhay <abhayetwup123@gmail.com>
2024-02-06 22:03:45 +0530

    loading-solved

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,62 +1,88 @@
-import React, {useEffect, useState} from 'react'
-import appwriteService from "../appwrite/config";
-import {Container, PostCard} from '../components'
+import React, { useEffect, useState } from 'react';
+import appwriteService from '../appwrite/config';
+import { Container, PostCard } from '../components';
 
 function Home() {
-    const [posts, setPosts] = useState([])
+    const [posts, setPosts] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
     useEffect(() => {
-        appwriteService.getPosts().then((posts) => {
-            if (posts) {
-                setPosts(posts.documents)
+        // Check if the user is logged in
+        appwriteService.isLoggedIn().then((loggedIn) => {
+            setIsLoggedIn(loggedIn);
+            if (loggedIn) {
+                // If logged in, fetch posts
+                appwriteService.getPosts().then((posts) => {
+                    if (posts) {
+                        setPosts(posts.documents);
+                        setLoading(false);
+                    }
+                });
+            } else {
+                // If not logged in, no need to fetch posts
                 setLoading(false);
             }
-        })
-    }, [])
+        });
+    }, []);
 
     if (loading) {
         return (
-          <div className="w-full py-8 mt-4 text-center min-h-screen">
-            <Container>
-              <div className="flex flex-wrap">
-                <div className="p-2 w-full h-full">
-                  <h1 className="text-2xl font-bold hover:text-gray-500">Loading...</h1>
-                </div>
-              </div>
-            </Container>
-          </div>
+            <div className="w-full py-8 mt-4 text-center min-h-screen">
+                <Container>
+                    <div className="flex flex-wrap">
+                        <div className="p-2 w-full h-full">
+                            <h1 className="text-2xl font-bold hover:text-gray-500">Loading...</h1>
+                        </div>
+                    </div>
+                </Container>
+            </div>
         );
-      }
-  
-    if (posts.length === 0) {
+    }
+
+    if (!isLoggedIn) {
         return (
             <div className="w-full py-8 mt-4 text-center min-h-screen">
                 <Container>
                     <div className="flex flex-wrap">
                         <div className="p-2 w-full h-full">
                             <h1 className="text-2xl font-bold hover:text-gray-500">
-                                Login To Create Post
+                                Please log in to view posts
                             </h1>
                         </div>
                     </div>
                 </Container>
             </div>
-        )
+        );
     }
+
+    if (posts.length === 0) {
+        return (
+            <div className="w-full py-8 mt-4 text-center min-h-screen">
+                <Container>
+                    <div className="flex flex-wrap">
+                        <div className="p-2 w-full h-full">
+                            <h1 className="text-2xl font-bold hover:text-gray-500">No posts available</h1>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+        );
+    }
+
     return (
-        <div className='w-full py-8'>
+        <div className="w-full py-8">
             <Container>
-                <div className='flex flex-wrap'>
+                <div className="flex flex-wrap">
                     {posts.map((post) => (
-                        <div key={post.$id} className='p-2 w-1/4'>
+                        <div key={post.$id} className="p-2 w-1/4">
                             <PostCard {...post} />
                         </div>
                     ))}
                 </div>
             </Container>
         </div>
-    )
+    );
 }
 
-export default Home
+export default Home;
